d6fc11675f685e7cf9013307cb63bfece4d7c100
Arthur <arthurhenrrique1705@gmail.com>
2025-03-15 19:19:15 -0300

    Commit

--- Aulas_JavaScript/Aula_3/Exercicios/Lista de tarefas/Script.js
+++ Aulas_JavaScript/Aula_3/Exercicios/Lista de tarefas/Script.js
@@ -1,46 +1,56 @@
 const url = "http://localhost:3000";
-            
-            async function atualizarLista() {
-                const response = await fetch(`${url}/tarefas`);
-                const novalista = await response.json();
-                const lista = document.getElementById("lista");
-                
-                lista.innerHTML = "";
 
-                novalista.forEach(task => {
-                    let novaTarefa = lista.insertRow();
-                    let checkbox = document.createElement("input");
-                    
-                    checkbox.type = "checkbox";
+async function atualizarLista() {
+    const response = await fetch(`${url}/tarefas`);
+    const novalista = await response.json();
+    const lista = document.getElementById("lista");
 
-                    novaTarefa.insertCell(0).innerHTML = `<input type="checkbox">`;
-                    novaTarefa.insertCell(1).textContent = task.titulo;
-                    novaTarefa.insertCell(2).textContent = task.descricao;
-                    novaTarefa.insertCell(3).textContent = (task.data == null) ? "Sem data especificada": task.data;
-                    novaTarefa.insertCell(4).textContent = (task.hora == null) ? "Sem hora especificada": task.hora;
+    lista.innerHTML = "";
 
-                    lista.appendChild(novaTarefa);
-                });
+    novalista.forEach(task => {
+        let novaTarefa = lista.insertRow();
+        let checkbox = document.createElement("input");
+        let botaoExcluir = document.createElement("button")
+        let botaoEditar = document.createElement("button")
 
-            }
-            
-            async function adicionarTarefa() {
-                const titulo = document.getElementById("titulo").value;
-                const descricao = document.getElementById("descricao").value;
-                const data = document.getElementById("data").value;
-                const hora = document.getElementById("hora").value;
+        botaoEditar.textContent = "Editar"
+        botaoExcluir.textContent = "Excluir";
+        
+        botaoEditar.classList = "botaoEditar"
+        botaoExcluir.classList = "botaoExcluir"
 
-                const response = await fetch((`${url}/tarefas`), {
-                    method: "POST",
-                    headers: { "Content-Type": "application/json" },
-                    body: JSON.stringify({ titulo, descricao, data, hora })
-                });
+        checkbox.type = "checkbox";
 
-                if (response.ok){
-                    atualizarLista()
-                } else {
-                    alert("Erro ao adicionar tarefa a lista!");
-                }
-            }
+        novaTarefa.insertCell().appendChild(checkbox);
+        novaTarefa.insertCell().textContent = task.titulo;
+        novaTarefa.insertCell().textContent = task.descricao;
+        novaTarefa.insertCell().textContent = (task.data == null) ? "Sem data especificada" : task.data;
+        novaTarefa.insertCell().textContent = (task.hora == null) ? "Sem hora especificada" : task.hora;
+        novaTarefa.insertCell().appendChild(botaoExcluir)
+        novaTarefa.insertCell().appendChild(botaoEditar)
 
-            atualizarLista();
+        lista.appendChild(novaTarefa);
+    });
+
+}
+
+async function adicionarTarefa() {
+    const titulo = document.getElementById("titulo").value;
+    const descricao = document.getElementById("descricao").value;
+    const data = document.getElementById("data").value;
+    const hora = document.getElementById("hora").value;
+
+    const response = await fetch((`${url}/tarefas`), {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ titulo, descricao, data, hora })
+    });
+
+    if (response.ok) {
+        atualizarLista()
+    } else {
+        alert("Erro ao adicionar tarefa a lista!");
+    }
+}
+
+atualizarLista();
