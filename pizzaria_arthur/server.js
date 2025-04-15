05c1e44b5672c3714674e94695e57d8ba787da7b
Arthur <arthurhenrrique1705@gmail.com>
2025-04-15 13:15:29 -0300

    Commit

--- pizzaria_arthur/server.js
+++ pizzaria_arthur/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const sqlite3 = require("sqlite3").verbose();
+const sqlite = require("sqlite3").verbose();
 
 const app = express();
 
@@ -8,8 +8,29 @@ app.use(cors());
 app.use(express.static("C:/Users/Aluno/Desktop/pizzaria_arthur/frontend"));
 app.use(express.json());
 
-const db = new sqlite3.Database("./database.db");
+const db = new sqlite.Database("./database.db", (err) => {
+    if(err) return console.log("Erro ao criar banco de dados!");
+    console.log("Banco de dados criado com exito!")
+});
+
 const PORT = 3000;
+
+db.run(`CREATE TABLE clientes (
+        id INTEGER PRIMARY KEY AUTO INCREMENT,
+        nome TEXT NOT NULL,
+        endereco TEXT NOT NULL,
+        telefone TEXT NOT NULL UNIQUE,
+        email TEXT NOT NULL UNIQUE
+    )`, (err) => {
+        if (err) return console.log("Erro ao criar tabela de clientes")
+    });
+
+app.get("/clientes", (req, res) => {
+    db.all(`SELECT * FROM clientes`, [], (err, rows) => {
+        if (err) return res.status(400).json({ error: err.message });
+        res.json(rows);
+    })
+})
 
 app.listen(PORT, (req, res) => {
     console.log("Servidor ouvindo em http://localhost:3000");
